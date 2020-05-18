f36dabde54ce86ee32e52d1738ad4dfdd0d525b9
Oleksiy Oletsky <oletsky@ukr.net>
2020-05-18 21:19:43 +0300

    Add files via upload

---
+++ demolist.py
@@ -0,0 +1,18 @@
+#Lists in Python
+
+l=[10,20,30,40,50]
+print(len(l))
+#Simple output
+print(l)
+
+#Output by loop
+for v in l:
+    print(v)
+
+#Merging lists
+l+=[60,70,80,90,100]
+print(l)
+print("------------------")
+#Main functions
+print("Max element is",max(l))
+print("Sum is ",sum(l))
